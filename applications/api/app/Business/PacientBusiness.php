49fa408f2e725169f9da3abd7a5cd65dad5a29c4
Joao Henrique <joaohenriquefsf@gmail.com>
2023-03-26 01:14:58 -0300

    creating the business class for patient

---
+++ applications/api/app/Business/PacientBusiness.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Business;
+
+use Illuminate\Http\JsonResponse;
+
+class PacientBusiness
+{
+    public function index(): JsonResponse
+    {
+        return response()->json();
+    }
+
+    public function show()
+    {
+        return response()->json();
+    }
+
+    public function store()
+    {
+        return response()->json();
+    }
+
+    public function update()
+    {
+        return response()->json();
+    }
+
+    public function delete()
+    {
+        return response()->json();
+    }
+}
