54be85258b68a5f0c7c5ea25685a14f75e4ed68d
Joao Henrique <joaohenriquefsf@gmail.com>
2023-03-26 03:00:56 -0300

    fixing the controller name in api routes

--- applications/api/routes/api.php
+++ applications/api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\PacientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('/patient', UserController::class);
+Route::resource('/patient', PacientController::class);
